0cb761d7227649e01462c83617482ee0ca4bc32a
zura tetrashvili <zuratetra@gmail.com>
2025-03-02 17:48:36 +0400

    add Static text

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import Info from "./Info.json";
+
 function App() {
   return (
     <>
-      <div className=""></div>
+      <div className="">
+        {Info.map((list, index) => (
+          <div className="" key={index}>
+            <h1 className="text-2xl">{list.title}</h1>
+            <p className="mt-4">{list.text}</p>
+            <p className="mt-5 underline">{list.author}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
